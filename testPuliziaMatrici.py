f198901edda7199481329c7e3e37ac8bd7798c22
Alessandro Andro <pasandro.pasandro@gmail.com>
2021-11-17 23:40:34 +0100

    Initial commit

---
+++ testPuliziaMatrici.py
@@ -0,0 +1,119 @@
+import numpy as np
+
+def getColumn(matr,i):
+    return [row[i] for row in matr]
+
+
+def rotateBy90(matr):
+    newMatrice = []
+    horizontalSize = np.shape(matr)[1]
+    for i in range(horizontalSize):
+        column = getColumn(matr, i)
+        #? lo mette al contrario https://stackoverflow.com/questions/6771428/most-efficient-way-to-reverse-a-numpy-array
+        column = column[::-1]
+        if len(newMatrice)==0:
+            #newMatrice = ([getColumn(matr, i)])
+            newMatrice = [column]
+        else:
+            #newMatrice.append(getColumn(matr, i))
+            newMatrice.append(column)
+    return newMatrice
+
+
+def verticalCleaner(matrice):
+    newMatrice = matrice
+    horizontalSize = np.shape(matrice)[1]
+    
+    #array righe da eliminare prima e dopo aver trovato un pattern
+    rowsToBeDeletedBefore = []
+    rowsToBeDeletedAfter = []
+
+    #checker per il controllo della presenza di un
+    prePatternFound = False
+
+    for i in range(horizontalSize):
+        #contrlla se ci sono 0
+        #! forse è meglio creare un ciclo
+        if(not np.any(getColumn(matrice,i))):
+            if(not prePatternFound):
+                rowsToBeDeletedBefore.append(i)            
+            else:
+                rowsToBeDeletedAfter.append(i)
+        else:
+            if(not prePatternFound):
+                prePatternFound = True
+            else:
+                rowsToBeDeletedAfter = []
+
+    totalToBeDeleted = rowsToBeDeletedBefore
+    totalToBeDeleted.extend(rowsToBeDeletedAfter)
+
+    #? cancella orizzontalmente
+    newMatrice = np.delete(matrice,totalToBeDeleted,1)
+    return newMatrice
+
+def patternCleaner(matrice):
+    newMatrice = matrice
+    #array righe da eliminare prima e dopo aver trovato un pattern
+    rowsToBeDeletedBefore = []
+    rowsToBeDeletedAfter = []
+
+    #checker per il controllo della presenza di un
+    prePatternFound = False
+
+    """
+    shape returna la dimesnione del vettore n dimensinato
+    sottforma di array di dimensioni in cui
+    - la prima posizione è uguale all adimensine in altezza della matrice
+    (la dimensione dell'array contenitore)
+    - in seconda posizione inddica la dimensnine degli array contenuti
+    all'interno
+    """
+
+    #! horizontal
+    #? conto le righe vuote e le aggiungo all'array delle righe da cancellare
+    for i in range(len(matrice)):
+        #contrlla se ci sono 0
+        #! forse è meglio creare un ciclo https://thispointer.com/6-ways-to-check-if-all-values-in-numpy-array-are-zero-in-both-1d-2d-arrays-python/
+        if(not np.any(matrice[i])):
+            if(not prePatternFound):
+                rowsToBeDeletedBefore.append(i)            
+            else:
+                rowsToBeDeletedAfter.append(i)
+        else:
+            if(not prePatternFound):
+                prePatternFound = True
+            else:
+                rowsToBeDeletedAfter = []
+    
+    totalToBeDeleted = rowsToBeDeletedBefore
+    totalToBeDeleted.extend(rowsToBeDeletedAfter)
+
+    #? cancella orizzontalmente
+    newMatrice = np.delete(matrice,totalToBeDeleted,0)
+
+    return newMatrice
+
+def deleteRow(matr, obj):
+    matr = np.delete(matr,(0,1),1)
+    print(matr)
+
+
+matrTest1 = ([
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,0],
+])
+horizontalClean = patternCleaner(matrTest1)
+verticalClean = verticalCleaner(horizontalClean)
+
+print(verticalClean)
+rotated = rotateBy90(verticalClean)
+print(rotated)
+rotated = rotateBy90(rotated)
+print(rotated)
+rotated = rotateBy90(rotated)
+print(rotated)
+
+
+#patternCleaner(matrTest1)
