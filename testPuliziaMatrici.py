faa7dce3e1a866ab5a57cc39fdffa608662776fa
MattiaCarlino <mattia.carlino@edu.unito.it>
2021-11-18 20:02:29 +0100

    sistemato la sintassi

--- testPuliziaMatrici.py
+++ testPuliziaMatrici.py
@@ -1,71 +1,75 @@
 import numpy as np
 
-def getColumn(matr,i):
+
+def get_column(matr, i):
     return [row[i] for row in matr]
 
-def matrixPrinter(matr):
+
+def matrix_printer(matr):
     for i in range(len(matr)):
         for j in range(len(matr[i])):
             print(matr[i][j], end=" ")
-        print(end='\n')
-    print('\n')
+        print(end="\n")
+    print("\n")
 
-def rotateBy90(matr):
-    newMatrice = []
-    horizontalSize = np.shape(matr)[1]  #?prende la lunghezza in orizontale
-    for i in range(horizontalSize):
-        column = getColumn(matr, i)
-        #? lo mette al contrario https://stackoverflow.com/questions/6771428/most-efficient-way-to-reverse-a-numpy-array
+
+def roatete_by_90(matr):
+    new_matrix = []
+    horizontal_size = np.shape(matr)[1]  # ?prende la lunghezza in orizontale
+    for i in range(horizontal_size):
+        column = get_column(matr, i)
+        # ? lo mette al contrario https://stackoverflow.com/questions/6771428/most-efficient-way-to-reverse-a-numpy-array
         column = column[::-1]
-        if len(newMatrice)==0:
-            #newMatrice = ([getColumn(matr, i)])
-            newMatrice = [column]
+        if len(new_matrix) == 0:
+            # new_matrix = ([get_column(matr, i)])
+            new_matrix = [column]
         else:
-            #newMatrice.append(getColumn(matr, i))
-            newMatrice.append(column)
-    return newMatrice
+            # new_matrix.append(get_column(matr, i))
+            new_matrix.append(column)
+    return new_matrix
 
 
-def verticalCleaner(matrice):
-    newMatrice = matrice
-    horizontalSize = np.shape(matrice)[1]
-    
-    #array righe da eliminare prima e dopo aver trovato un pattern
-    rowsToBeDeletedBefore = []
-    rowsToBeDeletedAfter = []
+def vertical_cleaner(matrice):
+    new_matrix = matrice
+    horizontal_size = np.shape(matrice)[1]
 
-    #checker per il controllo della presenza di un
-    prePatternFound = False
+    # array righe da eliminare prima e dopo aver trovato un pattern
+    rows_to_be_deleted_before = []
+    rows_to_be_deleted_after = []
 
-    for i in range(horizontalSize):
-        #contrlla se ci sono 0
+    # checker per il controllo della presenza di un
+    pre_pattern_found = False
+
+    for i in range(horizontal_size):
+        # contrlla se ci sono 0
         #! forse è meglio creare un ciclo
-        if(not np.any(getColumn(matrice,i))):
-            if(not prePatternFound):
-                rowsToBeDeletedBefore.append(i)            
+        if not np.any(get_column(matrice, i)):
+            if not pre_pattern_found:
+                rows_to_be_deleted_before.append(i)
             else:
-                rowsToBeDeletedAfter.append(i)
+                rows_to_be_deleted_after.append(i)
         else:
-            if(not prePatternFound):
-                prePatternFound = True
+            if not pre_pattern_found:
+                pre_pattern_found = True
             else:
-                rowsToBeDeletedAfter = []
+                rows_to_be_deleted_after = []
 
-    totalToBeDeleted = rowsToBeDeletedBefore
-    totalToBeDeleted.extend(rowsToBeDeletedAfter)
+    total_to_be_deleted = rows_to_be_deleted_before
+    total_to_be_deleted.extend(rows_to_be_deleted_after)
 
-    #? cancella orizzontalmente
-    newMatrice = np.delete(matrice,totalToBeDeleted,1)
-    return newMatrice
+    # ? cancella orizzontalmente
+    new_matrix = np.delete(matrice, total_to_be_deleted, 1)
+    return new_matrix
 
-def patternCleaner(matrice):
-    newMatrice = matrice
-    #array righe da eliminare prima e dopo aver trovato un pattern
-    rowsToBeDeletedBefore = []
-    rowsToBeDeletedAfter = []
 
-    #checker per il controllo della presenza di un
-    prePatternFound = False
+def pattern_cleaner(matrice):
+    new_matrix = matrice
+    # array righe da eliminare prima e dopo aver trovato un pattern
+    rows_to_be_deleted_before = []
+    rows_to_be_deleted_after = []
+
+    # checker per il controllo della presenza di un
+    pre_pattern_found = False
 
     """
     shape returna la dimesnione del vettore n 
@@ -77,49 +81,55 @@ def patternCleaner(matrice):
     """
 
     #! horizontal
-    #? conto le righe vuote e le aggiungo all'array delle righe da cancellare
+    # ? conto le righe vuote e le aggiungo all'array delle righe da cancellare
     for i in range(len(matrice)):
-        #contrlla se ci sono 0
+        # contrlla se ci sono 0
         #! forse è meglio creare un ciclo https://thispointer.com/6-ways-to-check-if-all-values-in-numpy-array-are-zero-in-both-1d-2d-arrays-python/
-        if(not np.any(matrice[i])):
-            if(not prePatternFound):
-                rowsToBeDeletedBefore.append(i)            
+        if not np.any(matrice[i]):
+            if not pre_pattern_found:
+                rows_to_be_deleted_before.append(i)
             else:
-                rowsToBeDeletedAfter.append(i)
+                rows_to_be_deleted_after.append(i)
         else:
-            if(not prePatternFound):
-                prePatternFound = True
+            if not pre_pattern_found:
+                pre_pattern_found = True
             else:
-                rowsToBeDeletedAfter = []
-    
-    totalToBeDeleted = rowsToBeDeletedBefore
-    totalToBeDeleted.extend(rowsToBeDeletedAfter)
+                rows_to_be_deleted_after = []
 
-    #? cancella orizzontalmente
-    newMatrice = np.delete(matrice,totalToBeDeleted,0)
+    total_to_be_deleted = rows_to_be_deleted_before
+    total_to_be_deleted.extend(rows_to_be_deleted_after)
 
-    return newMatrice
+    # ? cancella orizzontalmente
+    new_matrix = np.delete(matrice, total_to_be_deleted, 0)
 
-def deleteRow(matr, obj):
-    matr = np.delete(matr,(0,1),1)
+    return new_matrix
+
+
+def delete_row(matr, obj):
+    matr = np.delete(matr, (0, 1), 1)
     print(matr)
 
 
-matrTest1 = ([
-    [1,0,1,0],
-    [0,0,0,0],
-    [0,1,0,0],
-])
-horizontalClean = patternCleaner(matrTest1)
-verticalClean = verticalCleaner(horizontalClean)
+matr_test1 = [
+    [1, 1, 1, 1, 0],
+    [1, 0, 0, 1, 0],
+    [1, 1, 1, 1, 0],
+    [1, 0, 0, 1, 0],
+]
+horizontalClean = pattern_cleaner(matr_test1)
+vertical_clean = vertical_cleaner(horizontalClean)
 
-matrixPrinter(verticalClean)
-rotated = rotateBy90(verticalClean)
-matrixPrinter(rotated)
-rotated = rotateBy90(rotated)
-matrixPrinter(rotated)
-rotated = rotateBy90(rotated)
-matrixPrinter(rotated)
+print("Cleared matrix:")
+matrix_printer(vertical_clean)
+rotated = roatete_by_90(vertical_clean)
+print("90-degrees matrix:")
+matrix_printer(rotated)
+rotated = roatete_by_90(rotated)
+print("180-degrees  matrix:")
+matrix_printer(rotated)
+rotated = roatete_by_90(rotated)
+print("270-degrees matrix:")
+matrix_printer(rotated)
 
 
-#patternCleaner(matrTest1)
+# pattern_cleaner(matr_test1)
